49ca46a3c93ef1d280248c218c8a228b5c8c86d3
ysadka <ysadka@gmail.com>
2016-04-26 11:06:18 -0700

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,19 @@ class UsersController < ApplicationController
 
   def admin
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     @requests = Request.all
 =======
+=======
+>>>>>>> Stashed changes
 <<<<<<< HEAD
     @requests = Request.all.limit(10)
 =======
     @requests = Request.all
 >>>>>>> 5f2aa5c5adf3f3b0a12e0867a200937fcf872663
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,19 @@ Rails.application.routes.draw do
   get '/request-thanks', to: 'statics#request-thanks'
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   get '/scuba/steve', to: 'users#admin'
 =======
+=======
+>>>>>>> Stashed changes
 <<<<<<< HEAD
   get 'scuba/steve', to: 'user#admin'
 =======
   get '/scuba/steve', to: 'users#admin'
 >>>>>>> 5f2aa5c5adf3f3b0a12e0867a200937fcf872663
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
   resources :requests
